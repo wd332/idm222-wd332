c57ba44de962d1b54f40f2f29485f6306d325415
wd332 <wd332@drexel.edu>
2025-04-03 13:41:49 -0700

    changed the navigation bar :sparkles:

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openNav() {            // opens the side navigation
+    document.getElementById("mysidenav").classList.add("open"); // applies open class css to it to open it
+}
+
+function closeNav() {           // closes the side navigation
+    document.getElementById("mysidenav").classList.remove("open"); // removes open class css to it to close it
+}
