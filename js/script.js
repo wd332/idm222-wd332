43d08ed6eb169f979ce841d7503fb2fb311713f9
wd332 <wd332@drexel.edu>
2025-05-20 08:15:54 -0700

    Added Mono and made some design changes

--- js/script.js
+++ js/script.js
@@ -5,3 +5,38 @@ function openNav() {            // opens the side navigation
 function closeNav() {           // closes the side navigation
     document.getElementById("mysidenav").classList.remove("open"); // removes open class css to it to close it
 }
+
+// create an object linked to the id
+let zara_obj = document.getElementById("zara-button");
+
+// add an event listener click to that object
+zara_obj.addEventListener("click", zara_info);
+
+// js open or go to url on click
+function zara_info(url) {
+    window.open('zara.html', '_self');
+}
+
+let mono_obj = document.getElementById("mono-button");
+
+mono_obj.addEventListener("click", mono_info);
+
+function mono_info(url) {
+    window.open('mono.html', '_self');
+}
+
+let dragon_obj = document.getElementById("dragon-button");
+
+dragon_obj.addEventListener("click", dragon_info);
+
+function dragon_info(url) {
+    window.open('dragonventure.html', '_self');
+}
+
+let phoenix_obj = document.getElementById("phoenix-button");
+
+phoenix_obj.addEventListener("click", phoenix_info);
+
+function phoenix_info(url) {
+    window.open('thephoenixlantern.html', '_self');
+}
