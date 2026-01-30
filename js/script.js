bdc6706b92b04d7d5b0414c73a35832e2ea218b0
wd332 <wd332@drexel.edu>
2026-01-30 08:58:37 -0800

    organized projects
    - added categories to section projects
    - redid navigation
    - added buttons to navigate to categories
    - added more projects

--- js/script.js
+++ js/script.js
@@ -14,37 +14,39 @@ function closeNav() {           // closes the side navigation
 //     window.open('rocketship.html', '_self');
 // }
 
-// create an object linked to the id
+// Creates a variable and stores the HTML button element in it
 let zara_obj = document.getElementById("zara-button");
 
-// add an event listener click to that object
-zara_obj.addEventListener("click", zara_info);
+// if it exists, add an event listener click to that object
+if (zara_obj) {
+    zara_obj.addEventListener("click", zara_info);
+}
 
 // js open or go to url on click
-function zara_info(url) {
+function zara_info() {
     window.open('zara.html', '_self');
 }
 
 let mono_obj = document.getElementById("mono-button");
-
-mono_obj.addEventListener("click", mono_info);
-
-function mono_info(url) {
+if (mono_obj) {
+    mono_obj.addEventListener("click", mono_info);
+}
+function mono_info() {
     window.open('mono.html', '_self');
 }
 
 let dragon_obj = document.getElementById("dragon-button");
-
-dragon_obj.addEventListener("click", dragon_info);
-
-function dragon_info(url) {
+if (dragon_obj) {
+    dragon_obj.addEventListener("click", dragon_info);
+}
+function dragon_info() {
     window.open('dragonventure.html', '_self');
 }
 
 let phoenix_obj = document.getElementById("phoenix-button");
-
-phoenix_obj.addEventListener("click", phoenix_info);
-
-function phoenix_info(url) {
+if (phoenix_obj) {
+    phoenix_obj.addEventListener("click", phoenix_info);
+}
+function phoenix_info() {
     window.open('thephoenixlantern.html', '_self');
 }
